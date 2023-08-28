06df000d57e5816dd6f5aa32843e420a9ad1135c
lam.duong <lam.duong@netpower.no>
2023-08-28 15:19:20 +0700

    [bugfix] Re-add migrations, add nullabletype mark on some methods.

--- IEM.Infrastructure/Migrations/20230828074855_InitCreate.Designer.cs
+++ IEM.Infrastructure/Migrations/20230828074855_InitCreate.Designer.cs
@@ -3,6 +3,7 @@ using IEM.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace IEM.Infrastructure.Migrations
 {
     [DbContext(typeof(ApplicationDatabaseContext))]
-    partial class ApplicationDatabaseContextModelSnapshot : ModelSnapshot
+    [Migration("20230828074855_InitCreate")]
+    partial class InitCreate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
